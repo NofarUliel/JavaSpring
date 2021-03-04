7ee7a0c444e4184753564677bd0cb48927d9073d
Nofar Uliel <nofaruliel@gmail.com>
2021-03-04 11:02:54 +0200

    add all directory

---
+++ spring-demo-annotitaions/src/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,23 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// get the bean from spring
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+		// call a method from the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// close the context
+		context.close();
+
+	}
+
+}
