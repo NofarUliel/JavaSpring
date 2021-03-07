207bd7bcd739fd25543919b1c415a8e9efcf3abc
Nofar Uliel <nofaruliel@gmail.com>
2021-03-07 11:27:09 +0200

    added filed injection

--- spring-demo-annotitaions/src/springdemo/TennisCoach.java
+++ spring-demo-annotitaions/src/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	
@@ -18,11 +19,12 @@ public class TennisCoach implements Coach {
 //	}
 	
 	//define a setter method for injection
-	@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">>TennisCoach : inside setFortuneService() ");
-		this.fortuneService = fortuneService;
-	}
+//	@Autowired
+//	public void setFortuneService(FortuneService fortuneService) {
+//		System.out.println(">>TennisCoach : inside setFortuneService() ");
+//		this.fortuneService = fortuneService;
+//	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
