dbe7e418d18889b5a2ec0ea63c9a9fe4d2031ec9
Nofar Uliel <nofaruliel@gmail.com>
2021-03-07 12:54:33 +0200

    added java class config

---
+++ spring-demo-annotitaions/src/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package springdemo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(SportConfig.class);
+
+		// get the bean from spring
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+		// call a method from the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+
+		// close the context
+		context.close();
+
+	}
+
+}
